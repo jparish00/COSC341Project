1ed94792d7f73e6cf4d104d0d26bd5e307de27ae
RDArtemis <zoe.dale0617@gmail.com>
2023-12-01 23:01:15 -0800

    added strings for buttons/views/hints for review pages

--- app/src/main/java/com/example/cosc341project/AddReview.java
+++ app/src/main/java/com/example/cosc341project/AddReview.java
@@ -4,13 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class AddReview extends AppCompatActivity {
+
+    int review_count;
+    TextView vendorName;
+    RadioGroup review_rating;
+    RadioButton s1,s2,s3,s4,s5;
+    EditText leave_review;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_review);
+
     }
 
     public void submitReview(View view){
@@ -18,6 +29,6 @@ public class AddReview extends AppCompatActivity {
     }
 
     public void back(View view){
-        //TODO implement back button functionality
+        finish();
     }
 }
