d2b99cd1c13f765b8cdd1b0bc3796f380d155e1f
RDArtemis <zoe.dale0617@gmail.com>
2023-12-02 19:57:47 -0800

    worked on review pages

--- app/src/main/java/com/example/cosc341project/AddReview.java
+++ app/src/main/java/com/example/cosc341project/AddReview.java
@@ -4,27 +4,33 @@ import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddReview extends AppCompatActivity {
 
-    int review_count;
+    int review_count, review;
     TextView vendorName;
     RadioGroup review_rating;
-    RadioButton s1,s2,s3,s4,s5;
+    RadioButton s1,s2,s3,s4,s5, rb;
     EditText leave_review;
-    String venName, custName;
+    String venName, custName, rv, reviewtext;
+
+    Resources res;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_review);
 
+        res = getResources();
         Intent intent = getIntent();
 
         leave_review.findViewById(R.id.add_review_input);
@@ -46,6 +52,24 @@ public class AddReview extends AppCompatActivity {
 
     public void submitReview(View view){
         //TODO save review text and rating. increment count for number of reviews vendor has to compute average
+
+        int selectedID = review_rating.getCheckedRadioButtonId();
+        if(selectedID==-1){
+           Toast.makeText(this, res.getString(R.string.invalid_review), Toast.LENGTH_SHORT);
+        }
+        else
+            rb.findViewById(selectedID);
+
+        rv = rb.getText().toString();
+        review = Integer.parseInt(rv);
+
+        reviewtext = leave_review.getText().toString();
+        review_count++;
+
+        //todo store review rating, count, reviewtext, custname under vendor name in text.
+
+
+
 
     }
 
