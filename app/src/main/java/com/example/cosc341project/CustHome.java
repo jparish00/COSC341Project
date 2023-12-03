a8b7fc71a27638f35013f63f5a9604e1f1ee835a
mahirr476 <rmahirr476@gmail.com>
2023-12-02 22:54:57 -0800

    Custhome navbar implemented

--- app/src/main/java/com/example/cosc341project/CustHome.java
+++ app/src/main/java/com/example/cosc341project/CustHome.java
@@ -1,14 +1,39 @@
 package com.example.cosc341project;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class CustHome extends AppCompatActivity {
+
+    //navbar code
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cust_home);
+
+        // hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        // tool bar
+        setSupportActionBar(toolbar);
+
+        // nav drawer menu
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 }
