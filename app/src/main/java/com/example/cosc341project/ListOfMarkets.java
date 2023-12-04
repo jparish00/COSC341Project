42cbcc359ec1eb7f0af5bd717f5eda2258383a42
mahirr476 <rmahirr476@gmail.com>
2023-12-03 21:17:13 -0800

    design changes

---
+++ app/src/main/java/com/example/cosc341project/ListOfMarkets.java
@@ -0,0 +1,33 @@
+package com.example.cosc341project;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import androidx.cardview.widget.CardView;
+
+public class ListOfMarkets {
+
+    public static void populateMarkets(Context context, LinearLayout marketContainer) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        for (int i = 0; i < 10; i++) {
+            // Inflate or create your custom card view layout
+            CardView cardView = (CardView) inflater.inflate(R.layout.market_card, marketContainer, false);
+
+            // Customize card view
+            TextView marketName = cardView.findViewById(R.id.market_name);
+            TextView marketAddress = cardView.findViewById(R.id.market_address);
+            RatingBar marketRating = cardView.findViewById(R.id.market_rating);
+
+            marketName.setText("Market " + (i + 1));
+            marketRating.setRating(i % 5 + 1); // Just for example, setting a rating
+            marketAddress.setText("Market Address");
+
+
+            // Add the card view to your container
+            marketContainer.addView(cardView);
+        }
+    }
+}
--- app/src/main/java/com/example/cosc341project/ViewMarket.java
+++ app/src/main/java/com/example/cosc341project/ViewMarket.java
@@ -3,6 +3,7 @@ package com.example.cosc341project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class ViewMarket extends AppCompatActivity {
 
@@ -12,5 +13,9 @@ public class ViewMarket extends AppCompatActivity {
         setContentView(R.layout.activity_view_market);
         NavSetup.setupDrawer(this, R.id.drawer_layout, R.id.nav_view, R.id.toolbar);
 
+        // code for inserting all stores in market
+        LinearLayout marketContainer = findViewById(R.id.marketContainer);
+        // Populating store cards
+        ListOfMarkets.populateMarkets(this, marketContainer);
     }
 }
